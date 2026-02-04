798ff6801c0e19c6b06f6649d818199517ba8ca2
popiol <opiola.piotr@gmail.com>
2026-02-05 00:12:10 +0100

    more random noise

--- src/agents/stockfish_agent.py
+++ src/agents/stockfish_agent.py
@@ -52,8 +52,8 @@ class StockfishAgent(TrainableAgent):
             else:
                 eval_raw = None
 
-            eval_val = self._convert_stockfish_eval(eval_raw) + random.gauss(0, 0.1)
-            decisive = abs(eval_val) + random.gauss(0, 0.1)
+            eval_val = self._convert_stockfish_eval(eval_raw) + random.gauss(0, 0.2)
+            decisive = abs(eval_val) + random.gauss(0, 0.2)
 
             results.append(
                 PredictionResult(
