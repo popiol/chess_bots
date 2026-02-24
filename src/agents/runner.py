8ddbf79b77e2ec7b5653caea66ecdde32da35927
popiol <opiola.piotr@gmail.com>
2026-02-24 14:12:54 +0100

    acombo agent

--- src/agents/runner.py
+++ src/agents/runner.py
@@ -469,8 +469,9 @@ def main() -> None:
     classpath_map = {
         # "NeuralNetworkAgent": "src.agents.neural_network_agent.NeuralNetworkAgent",
         # "StockfishAgent": "src.agents.stockfish_agent.StockfishAgent",
-        "HeuristicAgent": "src.agents.heuristic_agent.HeuristicAgent",
+        # "HeuristicAgent": "src.agents.heuristic_agent.HeuristicAgent",
         # "DecisionTreeAgent": "src.agents.decision_tree_agent.DecisionTreeAgent",
+        "ComboAgent": "src.agents.combo_agent.ComboAgent",
     }
     available_classnames = list(classpath_map.keys())
     base_url = "https://playbullet.gg"
