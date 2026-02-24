b88194a6409e3355764bd55b0f386259027612e7
popiol <opiola.piotr@gmail.com>
2026-02-24 19:49:13 +0100

    just stockfish

--- src/agents/runner.py
+++ src/agents/runner.py
@@ -468,10 +468,10 @@ class AgentRunner:
 def main() -> None:
     classpath_map = {
         # "NeuralNetworkAgent": "src.agents.neural_network_agent.NeuralNetworkAgent",
-        # "StockfishAgent": "src.agents.stockfish_agent.StockfishAgent",
+        "StockfishAgent": "src.agents.stockfish_agent.StockfishAgent",
         # "HeuristicAgent": "src.agents.heuristic_agent.HeuristicAgent",
         # "DecisionTreeAgent": "src.agents.decision_tree_agent.DecisionTreeAgent",
-        "ComboAgent": "src.agents.combo_agent.ComboAgent",
+        # "ComboAgent": "src.agents.combo_agent.ComboAgent",
     }
     available_classnames = list(classpath_map.keys())
     base_url = "https://playbullet.gg"
